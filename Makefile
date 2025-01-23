0b732f7ab0be50e429d907fa16e37de1d1cdf169
ithemask <ai77xxxx@gmail.com>
2025-01-23 16:57:02 +0300

    Configure the project

---
+++ Makefile
@@ -0,0 +1,12 @@
+install:
+	uv sync
+makemigrations:
+	uv run python manage.py makemigrations
+migrate:
+	uv run python manage.py migrate
+start-dev:
+	uv run python manage.py runserver
+start-prod:
+	uv run python -m gunicorn task_manager.asgi:application -k uvicorn.workers.UvicornWorker
+lint:
+	uv run ruff check
---
+++ task_manager/settings.py
@@ -0,0 +1,99 @@
+import dj_database_url
+from pathlib import Path
+from os import getenv
+from os.path import join
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = getenv('SECRET_KEY')
+
+DEBUG = getenv('DEBUG')
+
+ALLOWED_HOSTS = [
+    'webserver',
+    '127.0.0.1',
+]
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'task_manager',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'task_manager.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'task_manager.wsgi.application'
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=getenv('DATABASE_URL'),
+        conn_max_age=600,
+    ),
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+
+if not DEBUG:
+    STATIC_ROOT = join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

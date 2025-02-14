2fb426ab9b6c15a254b0a11d4f7bd822e2ce5b71
ithemask <ai77xxxx@gmail.com>
2025-02-14 12:57:17 +0300

    Update urls.py module
    Add '/login', '/logout', '/users', '/users/create', '/users/<pk>/update', '/users/<pk>/delete', '/statuses', '/labels', '/tasks' URLs

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -4,6 +4,58 @@ from task_manager import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.IndexView.as_view(), name='index'),
+    path(
+        'admin/',
+        admin.site.urls,
+    ),
+    path(
+        '',
+        views.IndexView.as_view(),
+        name='index',
+    ),
+    path(
+        'login/',
+        views.UserLoginView.as_view(),
+        name='login',
+    ),
+    path(
+        'logout/',
+        views.UserLogoutView.as_view(),
+        name='logout',
+    ),
+    path(
+        'users/',
+        views.UserListView.as_view(),
+        name='user-list',
+    ),
+    path(
+        'users/create/',
+        views.UserCreateView.as_view(),
+        name='user-create',
+    ),
+    path(
+        'users/<int:pk>/update/',
+        views.UserUpdateView.as_view(),
+        name='user-update',
+    ),
+    path(
+        'users/<int:pk>/delete/',
+        views.UserDeleteView.as_view(),
+        name='user-delete',
+    ),
+    path(
+        'statuses/',
+        views.TemplateView.as_view(),
+        name='status-list',
+    ),
+    path(
+        'labels/',
+        views.TemplateView.as_view(),
+        name='label-list',
+    ),
+    path(
+        'tasks/',
+        views.TemplateView.as_view(),
+        name='task-list',
+    ),
 ]

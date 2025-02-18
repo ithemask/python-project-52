3fc970e8d8c2a4b3164b8532b741ac62dace49ff
ithemask <ai77xxxx@gmail.com>
2025-02-18 20:13:04 +0300

    Update urls.py module
    Add '/statuses/create', '/statuses/<pk>/update', '/statuses/<pk>/delete' URLs

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -45,8 +45,23 @@ urlpatterns = [
     ),
     path(
         'statuses/',
-        views.TemplateView.as_view(),
+        views.StatusListView.as_view(),
         name='status-list',
+    ),
+    path(
+        'statuses/create/',
+        views.StatusCreateView.as_view(),
+        name='status-create',
+    ),
+    path(
+        'statuses/<int:pk>/update/',
+        views.StatusUpdateView.as_view(),
+        name='status-update',
+    ),
+    path(
+        'statuses/<int:pk>/delete/',
+        views.StatusDeleteView.as_view(),
+        name='status-delete',
     ),
     path(
         'labels/',

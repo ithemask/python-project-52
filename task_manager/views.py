7c92f7f080385768b6116c71233ec5fa8d0a518b
ithemask <ai77xxxx@gmail.com>
2025-02-26 18:36:29 +0300

    Update views.py module
    Modify StatusDeleteView class: add StatusDeleteMixin class to the inheritance list; add TaskListView, TaskCreateView, TaskDetailView, TaskUpdateView, TaskDeleteView classes

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,10 +1,9 @@
 from django.contrib import messages
-from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from task_manager import forms, mixins, models
 
@@ -30,13 +29,13 @@ class UserLogoutView(LogoutView):
 
 
 class UserListView(ListView):
-    model = User
+    model = models.User
     ordering = 'id'
     template_name = 'user_list.html'
 
 
 class UserCreateView(SuccessMessageMixin, CreateView):
-    form_class = forms.SignUpForm
+    form_class = forms.UserCreateForm
     template_name = 'user_create.html'
 
     success_url = reverse_lazy('login')
@@ -49,7 +48,7 @@ class UserUpdateView(
     SuccessMessageMixin,
     UpdateView,
 ):
-    model = User
+    model = models.User
     form_class = forms.UserUpdateForm
     template_name = 'user_update.html'
 
@@ -63,7 +62,7 @@ class UserDeleteView(
     SuccessMessageMixin,
     DeleteView,
 ):
-    model = User
+    model = models.User
     template_name = 'user_delete.html'
 
     success_url = reverse_lazy('user-list')
@@ -103,6 +102,7 @@ class StatusUpdateView(
 
 class StatusDeleteView(
     mixins.AuthRequiredMixin,
+    mixins.StatusDeleteMixin,
     SuccessMessageMixin,
     DeleteView,
 ):
@@ -111,3 +111,75 @@ class StatusDeleteView(
 
     success_url = reverse_lazy('status-list')
     success_message = _('Status has been successfully deleted')
+
+
+class TaskListView(mixins.AuthRequiredMixin, ListView):
+    model = models.Task
+    ordering = 'id'
+    template_name = 'task_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = forms.TaskFilterForm(initial=self.request.GET)
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        status = self.request.GET.get('status')
+        executor = self.request.GET.get('executor')
+        self_tasks = self.request.GET.get('self_tasks')
+        if self_tasks:
+            queryset = queryset.filter(author=self.request.user)
+        if status:
+            queryset = queryset.filter(status=status)
+        if executor:
+            queryset = queryset.filter(executor=executor)
+        return queryset
+
+
+class TaskCreateView(
+    mixins.AuthRequiredMixin,
+    SuccessMessageMixin,
+    CreateView,
+):
+    form_class = forms.TaskForm
+    template_name = 'task_create.html'
+
+    success_url = reverse_lazy('task-list')
+    success_message = _('Task has been successfully created')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        return super().form_valid(form)
+
+
+class TaskDetailView(mixins.AuthRequiredMixin, DetailView):
+    model = models.Task
+    template_name = 'task_detail.html'
+
+
+class TaskUpdateView(
+    mixins.AuthRequiredMixin,
+    SuccessMessageMixin,
+    UpdateView,
+):
+    model = models.Task
+    form_class = forms.TaskForm
+    template_name = 'task_update.html'
+
+    success_url = reverse_lazy('task-list')
+    success_message = _('Task has been successfully updated')
+
+
+class TaskDeleteView(
+    mixins.AuthRequiredMixin,
+    mixins.TaskDeleteMixin,
+    SuccessMessageMixin,
+    DeleteView,
+):
+    model = models.Task
+    template_name = 'task_delete.html'
+
+    success_url = reverse_lazy('task-list')
+    success_message = _('Task has been successfully deleted')

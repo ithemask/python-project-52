2065706810e7b19b8910a4e4f03cf6d367e21029
ithemask <ai77xxxx@gmail.com>
2025-02-26 15:46:29 +0300

    Update models.py module
    Modify Status class: change string object representation; add custom User class; add Task class

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,6 +1,65 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    first_name = models.CharField(
+        _('first name'),
+        max_length=150,
+    )
+    last_name = models.CharField(
+        _('last name'),
+        max_length=150,
+    )
+
+    def __str__(self):
+        return self.get_full_name()
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        related_name='author_tasks',
+        null=True,
+        blank=True,
+    )
+    executor = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        related_name='executor_tasks',
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return self.name

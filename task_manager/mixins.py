d26b71d20f6ede4f05f92f7c4ece6c07b309f21d
ithemask <ai77xxxx@gmail.com>
2025-02-26 17:54:56 +0300

    Update mixins.py module
    Modify UserEditMixin class: add user deletion constraint handling; add StatusDeleteMixin, TaskDeleteMixin classes

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import ProtectedError
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
@@ -18,16 +19,48 @@ class AuthRequiredMixin(LoginRequiredMixin):
 
 
 class UserEditMixin:
-    error_message = _('You have no permission to edit another user.')
+    edit_error_message = _('You have no permission to edit another user.')
+    delete_error_message = _('User cannot be deleted because it is being used')
 
     def get(self, request, *args, **kwargs):
         if request.user != self.get_object():
-            messages.error(request, self.error_message)
+            messages.error(request, self.edit_error_message)
             return redirect('user-list')
         return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        if request.user != self.get_object():
-            messages.error(request, self.error_message)
+        try:
+            if request.user != self.get_object():
+                messages.error(request, self.edit_error_message)
+                return redirect('user-list')
+            return super().post(request, *args, **kwargs)
+        except ProtectedError:
+            messages.error(request, self.delete_error_message)
             return redirect('user-list')
+
+
+class StatusDeleteMixin:
+    error_message = _('Status cannot be deleted because it is being used')
+
+    def post(self, request, *args, **kwargs):
+        try:
+            return super().post(request, *args, **kwargs)
+        except ProtectedError:
+            messages.error(request, self.error_message)
+            return redirect('status-list')
+
+
+class TaskDeleteMixin:
+    error_message = _('Task can be deleted only by the author')
+
+    def get(self, request, *args, **kwargs):
+        if request.user != self.get_object().author:
+            messages.error(request, self.error_message)
+            return redirect('task-list')
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if request.user != self.get_object().author:
+            messages.error(request, self.error_message)
+            return redirect('task-list')
         return super().post(request, *args, **kwargs)

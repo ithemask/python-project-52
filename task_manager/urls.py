5b3125fe957a7240ea7f9aef98f016658b11eaf6
ithemask <ai77xxxx@gmail.com>
2025-02-26 15:28:18 +0300

    Update urls.py module
    Add '/tasks/create', '/tasks/<pk>', '/tasks/<pk>/update', '/tasks/<pk>/delete' URLs

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -70,7 +70,27 @@ urlpatterns = [
     ),
     path(
         'tasks/',
-        views.TemplateView.as_view(),
+        views.TaskListView.as_view(),
         name='task-list',
+    ),
+    path(
+        'tasks/create/',
+        views.TaskCreateView.as_view(),
+        name='task-create',
+    ),
+    path(
+        'tasks/<int:pk>/',
+        views.TaskDetailView.as_view(),
+        name='task-detail',
+    ),
+    path(
+        'tasks/<int:pk>/update/',
+        views.TaskUpdateView.as_view(),
+        name='task-update',
+    ),
+    path(
+        'tasks/<int:pk>/delete/',
+        views.TaskDeleteView.as_view(),
+        name='task-delete',
     ),
 ]

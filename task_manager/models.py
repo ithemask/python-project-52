8bd2341708d71c4e1d78edff4410a0ebf55ac36f
ithemask <ai77xxxx@gmail.com>
2025-02-27 23:26:02 +0300

    Update models.py module
    Add Label class; modify Task class: add 'labels' relationship field

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import ProtectedError
 from django.utils.translation import gettext_lazy as _
 
 
@@ -30,6 +31,24 @@ class Status(models.Model):
         return self.name
 
 
+class Label(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    def delete(self, *args, **kwargs):
+        if self.tasks.exists():
+            raise ProtectedError('', self)
+        super().delete(*args, **kwargs)
+
+
 class Task(models.Model):
     name = models.CharField(
         max_length=100,
@@ -55,6 +74,11 @@ class Task(models.Model):
         on_delete=models.PROTECT,
         related_name='executor_tasks',
         null=True,
+        blank=True,
+    )
+    labels = models.ManyToManyField(
+        Label,
+        related_name='tasks',
         blank=True,
     )
     created_at = models.DateTimeField(

7603a313cb20c12d890df2afdaa5acf6e76c866a
ithemask <ai77xxxx@gmail.com>
2025-02-27 23:22:33 +0300

    Update urls.py module
    Add '/labels/create', '/labels/<pk>/update', '/labels/<pk>/delete' URLs

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -65,8 +65,23 @@ urlpatterns = [
     ),
     path(
         'labels/',
-        views.TemplateView.as_view(),
+        views.LabelListView.as_view(),
         name='label-list',
+    ),
+    path(
+        'labels/create/',
+        views.LabelCreateView.as_view(),
+        name='label-create',
+    ),
+    path(
+        'labels/<int:pk>/update/',
+        views.LabelUpdateView.as_view(),
+        name='label-update',
+    ),
+    path(
+        'labels/<int:pk>/delete/',
+        views.LabelDeleteView.as_view(),
+        name='label-delete',
     ),
     path(
         'tasks/',
